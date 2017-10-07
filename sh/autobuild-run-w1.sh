7a0a84090551c88bc728b7603577b12cba0c94ef
upat <up4.3at@gmail.com>
2017-10-07 14:09:22 +0900

    私用スクリプトの追加

---
+++ sh/autobuild-run-w1.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+gcc='gcc -W -Wall'
+
+# 第1週レポート用 for MSYS2
+# Rドライブのディレクトリ下にある.cファイル(フルパスで出力)の検索
+for file in `\find /R/ -type f -name *.c`; do
+	# 検索したパスから拡張子を除去したファイル名を生成
+	fname=$(basename $file | sed 's/\.[^\.]*$//')
+	# 検索したパスからファイル名を含まないパスを生成
+	path=$(dirname $file)
+	# コンパイル後の実行ファイル名
+	output=$path/$fname.exe
+	# 実行結果の標準出力に使うテキストファイル名
+	txtfile=$path/$fname.log.txt
+
+	echo $file
+	$gcc $file -o $output
+	$output > $txtfile
+done
