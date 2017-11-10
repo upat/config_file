72ee4b81f1ca175cb432bc0f00efe02e39e66ed3
upat <up4.3at@gmail.com>
2017-11-10 17:45:49 +0900

    複数ファイルをコンパイルする時の挙動の見直しとログの着色

--- sh/autobuild-run-w1.sh
+++ sh/autobuild-run-w1.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 gcc='gcc -W -Wall'
+exe='_test.exe'
+exelog='_test.log.txt'
 
-# 第1週レポート用 for MSYS2
+# 第6週レポート用 for MSYS2
 # Rドライブのディレクトリ下にある.cファイル(フルパスで出力)の検索
 for file in `\find /R/ -type f -name *.c`; do
 	# 検索したパスから拡張子を除去したファイル名を生成
@@ -9,11 +11,27 @@ for file in `\find /R/ -type f -name *.c`; do
 	# 検索したパスからファイル名を含まないパスを生成
 	path=$(dirname $file)
 	# コンパイル後の実行ファイル名
-	output=$path/$fname.exe
+	output=$path/$exe
 	# 実行結果の標準出力に使うテキストファイル名
-	txtfile=$path/$fname.log.txt
+	txtfile=$path/$exelog
 
-	echo $file
-	$gcc $path/*.c -o $output
-	$output > $txtfile
+  # ログファイルが存在しない場合のみ実行
+  # （ソースファイル数と同じ回数コンパイルを繰り返すため）
+  if [ ! -e $txtfile ]; then
+	  echo -e "\e[36m$gcc $path/*.c -o $output\e[m"
+    echo -en "\e[31m"
+	  $gcc $path/*.c -o $output
+    echo -en "\e[m"
+    # 実行ファイルの生成に失敗した場合空のtxtを出力
+    if [ -e $output ]; then
+	    $output > $txtfile
+    else
+      echo -e "\e[35mexe file create failed.\e[m"
+      touch $txtfile
+    fi
+  else
+    echo -e "\e[32malready process.\e[m"
+  fi
 done
+echo -en "\e[m" #なんらかの理由でループが途切れた時用
+
